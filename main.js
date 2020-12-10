e1639b941bdd3b12127338afe09345df69796613
Ahmet Buğra Yiğiter <54077855+yigiterdev@users.noreply.github.com>
2020-12-10 14:14:10 +0300

    background and parallax effect

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ var swiper = new Swiper(".swiper-container", {
     prevEl: ".swiper-button-prev",
   },
 });
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+window.addEventListener("resize", () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
